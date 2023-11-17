92ff6f1307ce3021b2abb2260df19c32799de296
Merch7x <aquamaingi@gmail.com>
2023-11-17 20:34:57 +0300

    Call readfromstream from main

--- main.c
+++ main.c
@@ -3,23 +3,11 @@
 
 int main(int ac, char **av)
 {
-	FILE *bytecode;
-
 	if (ac == 1 || ac > 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	bytecode = fopen(av[1], "r");
-
-	if (bytecode == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		printf("Fucker is opening\n");
-	}
+	read_from_stream(av);
 	return (0);
 }
